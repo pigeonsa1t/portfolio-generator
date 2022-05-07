188b743068148350ef5137bd896226e04967f62d
pigeonsa1t <bowen_samantha@yahoo.com>
2022-05-07 18:36:05 -0400

    fixing missed line

--- app.js
+++ app.js
@@ -1,12 +1,6 @@
 const profileDataArgs = process.argv.slice(2, process.argv.length);
 console.log(profileDataArgs);   
 
-const printProfile = (profileDataArrs) => {
-    console.log(profileDataArrs);
-};
-
-printProfile(profileData);
-
 const printProfileData = (profileDataArr) => {
   console.log(profileDataArr);
 };
