65d1410ecd279a7b417e1a8fd04308fd4474fd7e
pigeonsa1t <bowen_samantha@yahoo.com>
2022-05-08 22:30:21 -0400

    pushing after code snap 9.2

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);   
+const fs = require('fs');
+const generatePage = require('./src/page-template');
 
-const printProfileData = (profileDataArr) => {
-    console.log(profileDataArr);
-};
+const profileDataArgs = process.argv.slice(2);
 
-printProfileData(profileDataArgs);
+console.log(profileDataArgs);
+
+const [name, github] = profileDataArgs;
+
+console.log(name, github);
+
+const pageHTML = generatePage(name, github);
+
+fs.writeFile('./index.html', pageHTML, err => {
+  if (err) throw err;
+
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
